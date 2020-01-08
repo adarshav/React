098f147dd6882ca63d6600566e97706003b58d74
adarsha <adarshav957@gmail.com>
2020-01-08 17:24:49 +0530

    Updates

---
+++ precache-manifest.559686d952993d8824610d82bc93e3d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5b326808a395772f3010dcd3892ba6",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "2a7350e9ddd5a19b2b8c",
+    "url": "/React/static/css/main.48310f05.chunk.css"
+  },
+  {
+    "revision": "7dd049b8f808525ebd13",
+    "url": "/React/static/js/2.31ea1a54.chunk.js"
+  },
+  {
+    "revision": "2a7350e9ddd5a19b2b8c",
+    "url": "/React/static/js/main.8e5aa8f1.chunk.js"
+  },
+  {
+    "revision": "6d4268a174e7a219dc4f",
+    "url": "/React/static/js/runtime-main.57ed7368.js"
+  }
+]);
